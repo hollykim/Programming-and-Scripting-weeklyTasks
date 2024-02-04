deef833fb4a1f80a23196d9ff43fbc8554a3d9ba
unknown <holly.hyojin@gmail.com>
2024-02-04 21:48:12 +0000

    weekly task 02

---
+++ bank.py
@@ -0,0 +1,10 @@
+# Weekly tasks 02
+# program that gets 2 money by cent from the user and prints by euro
+# Author Hyojin Kim 
+
+amount1 = float (input ("Enter amount1 (in cent): ")) / 100
+amount2 = float (input ("Enter amount2 (in cent): ")) / 100
+total = amount1 + amount2
+
+print (f"The sum of these is € {total}")
+
