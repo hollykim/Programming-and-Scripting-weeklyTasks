ef15553774c0d4272e64ec5c59d0665265757f19
Hyojin Kim <holly.hyoijn@gmail.com>
2024-02-17 21:52:30 +0000

    weekly task 04

--- collatz.py
+++ collatz.py
@@ -1,10 +1,8 @@
-# This program will ask to the user to put any positive integer and will give outpus 
-# as the successive values of the following calculation.
-# At each step calculate the next value by taking the current value and, if it is even, divide it by two, 
-# but if it is odd, multiply it by three and add one. This program end if the current value is one.
+# This program will ask to the user to put any positive integer and will give output as the successive values of the following calculation.
+# If the input is even, divide it by two, and if it is odd, multiply it by three and add one. 
+# This program end if the current value is one.
 # Author: Hyojin
 # ref: https://codereview.stackexchange.com/questions/285429/automate-the-boring-stuff-with-python-the-collatz-sequence#:~:text=Write%20a%20function%20named%20collatz,return%203%20*%20number%20%2B%201%20.
-
 
 num = int(input ("please enter a positive integer: "))
 
@@ -12,7 +10,7 @@ num = int(input ("please enter a positive integer: "))
 if num <= 0:
     print ("It's not a positive number")
 
-#if the user put number 1, the while loop won't work. So I added this condition to make sure it's run 
+# if the user put number 1, the while loop won't work. So I added this condition to make sure it works even the input is 1.
 elif num == 1:
     num = (num*3)+1 
     print (num, sep=" ", end=" ")
@@ -24,7 +22,7 @@ elif num == 1:
             num = int(num/2)
             print (num, sep=" ", end=" ")      
 else: 
-# if num is even will do num*3+1, if it's odd will do /2 and this loop will run until num is equal to 1.
+# if num is even, will do num*3+1. if it's odd, will do /2 and this loop will run until num is equal to 1.
     while num != 1:
         if num % 2 != 0:
             num = (num*3)+1
@@ -34,7 +32,7 @@ else:
             print (num, sep=" ", end=" ")        
 
 
-# The original code I made (without condition about input:1)
+# The original code I made (without condition about input is 1)
 
 # num1 = int(input ("please enter a positive integer: "))
 
@@ -50,7 +48,9 @@ else:
 #                 print (num1, sep=" ", end=" ")        
 
 
-# below is some cod I have tried. I thought while the num is equal to 1, it'll running but it was my misunderstanding.
+
+
+# below is some code I have tried. I thought while the num is equal to 1, it'll running but it was my misunderstanding.
         
 # while num == 1:     
 #     if num % 2 == 0:
